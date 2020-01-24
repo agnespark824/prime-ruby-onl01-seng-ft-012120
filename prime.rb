63e99f11ca5dab27b79bee0dcfcf418e1c86e376
Agnes Park <agnespark824@gmail.com>
2020-01-24 07:26:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,10 @@
 def prime?(number)
   if number.negative? 
-  array = (2...number).to_a
-  array.all? {|n| number % n != 0}
+    false
+  else
+    array = (2...number).to_a
+    array.all? {|n| number % n != 0}
+  end
 end
   
   #boolean
